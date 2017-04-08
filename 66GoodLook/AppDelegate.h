f29a8bc77802a399da582576c6530179ea4e53ca
XuYanci <grandy.wind@gmail.com>
2017-04-08 16:59:55 +0800

    first commit

---
+++ 66GoodLook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  66GoodLook
+//
+//  Created by Yanci on 17/3/14.
+//  Copyright © 2017年 Yanci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 66GoodLook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  66GoodLook
+//
+//  Created by Yanci on 17/3/14.
+//  Copyright © 2017年 Yanci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
