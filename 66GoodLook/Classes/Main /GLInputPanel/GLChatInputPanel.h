f2ebbdd92795672bbdf812760bee55826ea214f8
XuYanci <grandy.wind@gmail.com>
2017-04-20 20:07:49 +0800

    commit some

---
+++ 66GoodLook/Classes/Main /GLInputPanel/GLChatInputPanel.h
@@ -0,0 +1,28 @@
+//
+//  GLChatInputPanel.h
+//  66GoodLook
+//
+//  Created by Yanci on 17/4/20.
+//  Copyright © 2017年 Yanci. All rights reserved.
+//
+
+#import "GLChatInputBaseView.h"
+
+/*!
+ @class GLChatInputPanel
+ 
+ @brief The UIView class
+ 
+ @discussion    聊天输入面板
+ 
+ @superclass SuperClass: GLChatInputBaseView\n
+ @classdesign    No special design is applied here.
+ @coclass    None
+ @helps It helps no other classes.
+ @helper    No helper exists for this class.
+ */
+@interface GLChatInputPanel : GLChatInputBaseView
+
+@end
+
+
---
+++ 66GoodLook/Classes/Main /GLInputPanel/GLChatInputToolBar.h
@@ -0,0 +1,25 @@
+//
+//  GLChatInputToolBar.h
+//  66GoodLook
+//
+//  Created by Yanci on 17/4/20.
+//  Copyright © 2017年 Yanci. All rights reserved.
+//
+
+#import "GLChatInputBaseView.h"
+
+/*!
+ @class GLChatInputToolBar
+ 
+ @brief The UIView class
+ 
+ @discussion    聊天输入工具框
+ 
+ @superclass SuperClass: GLChatInputBaseView\n
+ @classdesign    No special design is applied here.
+ @coclass    None
+ @helps It helps no other classes.
+ @helper    No helper exists for this class.
+ */@interface GLChatInputToolBar : GLChatInputBaseView
+
+@end
