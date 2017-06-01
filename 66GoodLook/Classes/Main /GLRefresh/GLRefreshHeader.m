8467715206abc271771272ad7814259442d33f30
XuYanci <grandy.wind@gmail.com>
2017-06-01 18:50:52 +0800

    * commit some.

--- 66GoodLook/Classes/Main /GLRefresh/GLRefreshHeader.m
+++ 66GoodLook/Classes/Main /GLRefresh/GLRefreshHeader.m
@@ -8,6 +8,18 @@
 
 #import "GLRefreshHeader.h"
 
+static NSString * kStringImagePath1 = @"xialashuaxin-icon-2";
+static NSString * kStringImagePath2 = @"xialashuaxin-icon-3";
+static NSString * kStringImagePath3 = @"xialashuaxin-icon-4";
+static NSString * kStringImagePath4 = @"xialashuaxin-icon-5";
+static NSString * kStringImagePath5 = @"xialashuaxin-icon-6";
+
+@interface GLRefreshHeader()
+@property (nonatomic,strong) UIView *contentView;                 /** 容器视图 */
+@property (nonatomic,strong) UIImageView *backgroundView;         /** 背景视图 */
+@property (nonatomic,strong) UIImageView *stringView;             /** 绳子视图 */
+@end
+
 @implementation GLRefreshHeader
 
 /*
@@ -17,5 +29,62 @@
     // Drawing code
 }
 */
+- (void)prepare
+{
+    [super prepare];
+    
+    [self beginRefreshingWithCompletionBlock:^{
+        NSLog(@"animation begin");
+    }];
+    
+    [self endRefreshingWithCompletionBlock:^{
+        NSLog(@"animation end");
+    }];
+    
+    [self addSubview:self.contentView];
+    [self.contentView addSubview:self.backgroundView];
+    [self.contentView addSubview:self.stringView];
+    
+    [self.backgroundView setImage:[UIImage imageNamed:@"xialashuaxin-icon-1"]];
+    [self.stringView setImage:[UIImage imageNamed:kStringImagePath1]];
+    
+    [self.backgroundView sizeToFit];
+    self.mj_h = CGRectGetHeight(self.backgroundView.bounds);
+}
+
+- (void)placeSubviews
+{
+    [super placeSubviews];
+    self.contentView.frame = self.bounds;
+    self.backgroundView.frame = self.contentView.bounds;
+    [self.stringView sizeToFit];
+    [self.stringView setCenter:CGPointMake(60.0, CGRectGetHeight(self.stringView.bounds) / 2.0)];
+}
+
+
+#pragma mark - getter and setter
+- (UIView *)contentView {
+    if (!_contentView) {
+        _contentView = [[UIView alloc]init];
+    }
+    return _contentView;
+}
+
+- (UIImageView *)backgroundView {
+    if (!_backgroundView) {
+        _backgroundView = [[UIImageView alloc]init];
+        _backgroundView.contentMode = UIViewContentModeScaleAspectFit;
+    }
+    return _backgroundView;
+}
+
+- (UIImageView *)stringView {
+    if (!_stringView) {
+        _stringView = [[UIImageView alloc]init];
+        _stringView.contentMode = UIViewContentModeScaleAspectFit;
+    }
+    return _stringView;
+}
+
 
 @end
